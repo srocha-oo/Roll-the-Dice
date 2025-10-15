c73e8c9daecf4303665ea2a2af9ef4bf80118529
Stephanie Rocha M <srochamarquez@bsd220.org>
2025-10-15 10:49:07 -0500

    Initial Commit

---
+++ Roll the Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roll the Dice
+//
+//  Created by stephanie rocha marquez on 10/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by stephanie rocha marquez on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
